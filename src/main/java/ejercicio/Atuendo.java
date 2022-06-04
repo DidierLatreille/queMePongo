ac7fd9143dc370a13d8f289a87fa8f8b5a015625
DidierLatreille <didier.latreille@gmail.com>
2022-06-04 03:06:54 -0300

    atuendo.java

---
+++ src/main/java/ejercicio/Atuendo.java
@@ -0,0 +1,21 @@
+package ejercicio;
+
+public class Atuendo {
+	private Accesorio accesorio;
+	private ParteSuperior parteSuperior;
+	private ParteInferior parteInferior;
+	private Calzado calzado;
+	
+	public Atuendo(Accesorio newAcce, ParteSuperior newParteSup, ParteInferior newParteInf, Calzado newCalza) {
+		accesorio = newAcce;
+		
+		if(newParteSup == null) {throw new NullPointerException("La parte sup no puede quedar vacia");}
+		parteSuperior = newParteSup;
+		
+		if(newParteInf == null) {throw new NullPointerException("La parte inf no puede quedar vacia");}
+		parteInferior = newParteInf;
+		
+		if(newCalza == null) {throw new NullPointerException("El calzado no puede quedar vacio");}
+		calzado = newCalza;
+	}
+}
